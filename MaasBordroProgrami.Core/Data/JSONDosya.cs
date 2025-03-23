7d5437b8d01e1c4261a64b3a3bd7d7f9c96dc3ff
Berkay Arslan <arslanberkay18@gmail.com>
2025-03-23 18:48:47 +0300

    JSONDosya classına açıklamalar eklendi. Dizaynda iyileştirmeler yapıldı.

--- MaasBordroProgrami.Core/Data/JSONDosya.cs
+++ MaasBordroProgrami.Core/Data/JSONDosya.cs
@@ -17,50 +17,68 @@ namespace MaasBordroProgrami.Core.Data
 {
     public class JSONDosya
     {
+        //Bu class personel listesini JSON formatında saklamak,okumak ve belirli bir personelin maaş bordrosunu kaydetmek için oluşturulmuştur.
+
+        /// <summary>
+        /// Bu metod data.json dosyasından personel listesini okuyup BindingList<IPersonel> türünde bir liste olarak döndürür.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="Exception"></exception>
         public static BindingList<IPersonel> PersonelListesiOku()
         {
-            string projeDizini = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string hedefDizin = Path.Combine(projeDizini, @"..\..\..\", "Data");
+            //Dosya Yolunu Belirleme
+            string projeDizini = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); //Programın çalıştığı dizini bulur
+            string hedefDizin = Path.Combine(projeDizini, @"..\..\..\", "Data"); //Data klasöründeki data.json dosyasının tam yolunu oluşturur.
             string jsonDosyaYolu = Path.Combine(hedefDizin, "data.json");
 
-            if (!File.Exists(jsonDosyaYolu))
+            if (!File.Exists(jsonDosyaYolu)) //Dosya mevcut değilse hata fırlatır.
             {
                 throw new Exception("JSON dosyası bulunamadı.");
             }
 
-            string jsonVeri = File.ReadAllText(jsonDosyaYolu);
-            var hamListe = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(jsonVeri);
+            //JSON Verisin Okuma
+            string jsonVeri = File.ReadAllText(jsonDosyaYolu); //JSON dosyasının tamamını okur
+            var hamListe = JsonConvert.DeserializeObject<List<Dictionary<string, object>>>(jsonVeri); //JSON'u List<Dictionary<string,object>> türüne çevirir. (esnek bir yapı için)
 
-            var personelListesi = new BindingList<IPersonel>();
+            //Personel Tipine Göre Nesne Oluşturma
+            var personelListesi = new BindingList<IPersonel>(); //List benzeri bir yapıdır . Özellikle veri bağlama (data binding) işlemleri için geliştirilmiştir. Binding sayesinde listeye yeni bir personel eklendiğinde silindiğinde elle güncellemek zorunda kalmayız. Windows Forms projelerinde tercih edilir çünkü DataGridView, ListView gibi arayüz bileşenleriyle uyumlu,sorunsuz çalışır. List kullandığım zaman form bileşenleriyle kontak olması için çok fazla güncelleme işlemi yapmam gerekti. BindingList tüm bu işlemleri dinamikleştirdi.
 
+            //Amaç: JSON'dan gelen veriyi, projede tanımlanmış Memur,Yonetici sınıfına dönüştürüp BindingList içine eklemek.
             foreach (var item in hamListe)
             {
-                if (item.ContainsKey("Kadro") && item["Kadro"].ToString() == "Yönetici")
+                if (item.ContainsKey("Kadro") && item["Kadro"].ToString() == "Yönetici") //Kadro varsa ve Yönetici ise 
                 {
-                    var yonetici = JsonConvert.DeserializeObject<Yonetici>(JsonConvert.SerializeObject(item));
+                    var yonetici = JsonConvert.DeserializeObject<Yonetici>(JsonConvert.SerializeObject(item)); //item nesnesini JSON(string) formatına dönüştürür. Daha sonra JSON formatındaki veriyi Yonetici türünde bir nesneye çevirir.
                     personelListesi.Add(yonetici);
                 }
-                else if (item.ContainsKey("Kadro") && item["Kadro"].ToString() == "Memur")
+                else if (item.ContainsKey("Kadro") && item["Kadro"].ToString() == "Memur") //Kadro varsa ve Memur ise
                 {
-                    var memur = JsonConvert.DeserializeObject<Memur>(JsonConvert.SerializeObject(item));
+                    var memur = JsonConvert.DeserializeObject<Memur>(JsonConvert.SerializeObject(item)); //item nesnesini JSON(string) formatına dönüştürür. Daha sonra JSON formatındaki veriyi Memur türünde bir nesneye çevirir.
                     personelListesi.Add(memur);
                 }
             }
             return personelListesi;
         }
 
+        /// <summary>
+        /// Bu metod List<IPersonel> türündeki personel listesini JSON formatında data.json dosyasına kaydeder.
+        /// </summary>
+        /// <param name="personelListesi"></param>
         public static void PersonelListesineKaydet(List<IPersonel> personelListesi)
         {
-
+            //Dosya Yolu Oluşturma
             string projeDizini = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
             string hedefDizin = Path.Combine(projeDizini, @"..\..\..\", "Data");
             string jsonDosyaYolu = Path.Combine(hedefDizin, "data.json");
 
-            var jsonList = new List<object>();
+            //personelListesi'ni JSON Nesnesine Dönüştürme
+            var jsonListesi = new List<object>();
 
             foreach (var personel in personelListesi)
             {
-                jsonList.Add(new
+                //Her personel nesnesinden anonim bir nesne oluşturulur (new { } yapısı)
+                //Bu nesneler jsonListesi adlı listeye eklenir.
+                jsonListesi.Add(new
                 {
                     personel.AdSoyad,
                     personel.CalismaSaati,
@@ -69,29 +87,34 @@ namespace MaasBordroProgrami.Core.Data
                 });
             }
 
-            string jsonVeri = JsonConvert.SerializeObject(jsonList, Formatting.Indented);
-            File.WriteAllText(jsonDosyaYolu, jsonVeri);
+            string jsonVeri = JsonConvert.SerializeObject(jsonListesi, Formatting.Indented); //Nesneyi JSON formatına dönüştürür. Formatting.Indented : JSON çıktısını düzenli hale getirir.
+            File.WriteAllText(jsonDosyaYolu, jsonVeri); //JSON verisini belirtilen dosya yoluna yazar.
 
         }
 
+        /// <summary>
+        /// Bu metod personelin maaş bordrosunu kişinin adını taşıyan bir klasöre kaydeder.
+        /// </summary>
+        /// <param name="personelBordro"></param>
+        /// <param name="personelAdSoyad"></param>
         public static void PersonelAdinaKaydet(MaasBordro personelBordro, string personelAdSoyad)
         {
-
-            string masaUstuYolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            //Masaüstü Yolu ve Hedef Klasör Oluşturma
+            string masaUstuYolu = Environment.GetFolderPath(Environment.SpecialFolder.Desktop); //Kullanıcının masaüstü dizinini alır.
 
             string klasorYolu = Path.Combine(masaUstuYolu, personelAdSoyad.ToUpper());
-            if (!Directory.Exists(klasorYolu))
+            if (!Directory.Exists(klasorYolu)) //Klasör yoksa
             {
                 Directory.CreateDirectory(klasorYolu);
             }
 
+            //JSON Dosya Adı ve Yolu Oluşturma
             string dosyaYolu = Path.Combine(klasorYolu, $"{personelAdSoyad.ToLower()}bordro.json");
 
+            //Maaş Bordrsounu JSON Olarak Kaydetme
             var jsonAyarlar = new JsonSerializerOptions { WriteIndented = true };
-            string jsonPersonel = System.Text.Json.JsonSerializer.Serialize(personelBordro,jsonAyarlar);
-            File.WriteAllText(dosyaYolu, jsonPersonel);
-
-            
+            string jsonPersonel = System.Text.Json.JsonSerializer.Serialize(personelBordro, jsonAyarlar); //MaasBordro nesnesini JSON formatına dönüştürür
+            File.WriteAllText(dosyaYolu, jsonPersonel); //JSON çıktısı masaüstündeki ilgili klasöre kaydedilir.
         }
 
     }
